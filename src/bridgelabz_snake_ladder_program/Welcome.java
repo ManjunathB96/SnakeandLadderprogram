dfff09a490331999552d4256ecb44f5bb6164a4a
Manju31051996 <bbelagavi6@gmail.com>
2022-10-26 08:38:32 +0530

    add welcome file

---
+++ src/bridgelabz_snake_ladder_program/Welcome.java
@@ -0,0 +1,7 @@
+package bridgelabz_snake_ladder_program;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game...!");
+    }
+}
