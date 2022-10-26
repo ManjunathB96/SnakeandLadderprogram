3392cdfdc1de8d610511a255e69e09a6390d7deb
Manju31051996 <bbelagavi6@gmail.com>
2022-10-26 09:20:10 +0530

    [Manjunath]added the UC3 case file

--- src/bridgelabz_snake_ladder_program/Welcome.java
+++ src/bridgelabz_snake_ladder_program/Welcome.java
@@ -8,5 +8,24 @@ public class Welcome {
         int dieRolls= (int) ((Math.random() * 10) %6 + 1);
         System.out.println("Start Position is : "+startPosition);
         System.out.println("The Number of Die Rolls is : "+dieRolls);
+
+        double checkForOption= ((Math.random() *10) %3 +1);
+
+        switch ((int) checkForOption){
+            case 1:
+                System.out.println("Stay in Same Position");
+
+            case 2:
+                System.out.println("you got ladder : move ahead as per Die number");
+                startPosition=startPosition+dieRolls;
+                System.out.println(startPosition);
+
+            case 3:
+                System.out.println("got Snake : move behind as per Die Number");
+                startPosition = startPosition+dieRolls;
+                System.out.println(startPosition);
+            default:
+                System.out.println("No choice Stay In Same position");
+        }
     }
 }
