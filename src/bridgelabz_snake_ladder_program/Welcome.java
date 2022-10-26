bb49292d03e8bc7a6ce471470af08a4ceefd6226
Manju31051996 <bbelagavi6@gmail.com>
2022-10-26 08:44:32 +0530

    [Manjunath]added the UC1 case file

--- src/bridgelabz_snake_ladder_program/Welcome.java
+++ src/bridgelabz_snake_ladder_program/Welcome.java
@@ -3,5 +3,8 @@ package bridgelabz_snake_ladder_program;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game...!");
+
+        int startPosition = 0;
+        System.out.println("Start Position is : "+startPosition);
     }
 }
